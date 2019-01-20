98809101b885185830ae8a606630c7887c53fa1f
Albert Leon <aleon42@me.com>
2019-01-19 21:49:59 -0500

    first commit

---
+++ coderSwag/coderSwag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coderSwag
+//
+//  Created by Albert Leon on 1/19/19.
+//  Copyright © 2019 Albert Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
